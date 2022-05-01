c2affce6d357125a01b6e4cc5f03ae9bd88cf60c
Mathias Neerup <mdb@kmdb.dk>
2022-05-01 18:54:44 +0200

    Add wrapper for "old" auth

--- certbot_dns_azure/_internal/dns_azure.py
+++ certbot_dns_azure/_internal/dns_azure.py
@@ -16,6 +16,58 @@ from certbot.plugins import dns_common
 logger = logging.getLogger(__name__)
 logging.getLogger('azure').setLevel(logging.WARNING)
 
+# Wrap credentials from azure-identity to be compatible with SDK that needs msrestazure or azure.common.credentials
+# Need msrest >= 0.6.0
+# See also https://pypi.org/project/azure-identity/
+
+from msrest.authentication import BasicTokenAuthentication
+from azure.core.pipeline.policies import BearerTokenCredentialPolicy
+from azure.core.pipeline import PipelineRequest, PipelineContext
+from azure.core.pipeline.transport import HttpRequest
+
+from azure.identity import DefaultAzureCredential
+
+class CredentialWrapper(BasicTokenAuthentication):
+    def __init__(self, credential=None, resource_id="https://management.azure.com/.default", **kwargs):
+        """Wrap any azure-identity credential to work with SDK that needs azure.common.credentials/msrestazure.
+        Default resource is ARM (syntax of endpoint v2)
+        :param credential: Any azure-identity credential (DefaultAzureCredential by default)
+        :param str resource_id: The scope to use to get the token (default ARM)
+        """
+        super(CredentialWrapper, self).__init__(None)
+        if credential is None:
+            credential = DefaultAzureCredential()
+        self._policy = BearerTokenCredentialPolicy(credential, resource_id, **kwargs)
+
+        self.credential = credential
+    def _make_request(self):
+        return PipelineRequest(
+            HttpRequest(
+                "CredentialWrapper",
+                "https://fakeurl"
+            ),
+            PipelineContext(None)
+        )
+
+    def set_token(self):
+        """Ask the azure-core BearerTokenCredentialPolicy policy to get a token.
+        Using the policy gives us for free the caching system of azure-core.
+        We could make this code simpler by using private method, but by definition
+        I can't assure they will be there forever, so mocking a fake call to the policy
+        to extract the token, using 100% public API."""
+        request = self._make_request()
+        self._policy.on_request(request)
+        # Read Authorization, and get the second part after Bearer
+        token = request.http_request.headers["Authorization"].split(" ", 1)[1]
+        self.token = {"access_token": token}
+
+    def get_token(self, *scopes, **kwargs):
+        # Pass get_token call to credential
+        return self.credential.get_token(*scopes, **kwargs)
+
+    def signed_session(self, session=None):
+        self.set_token()
+        return super(CredentialWrapper, self).signed_session(session)
 
 @zope.interface.implementer(interfaces.IAuthenticator)
 @zope.interface.provider(interfaces.IPluginFactory)
@@ -265,4 +317,4 @@ class Authenticator(dns_common.DNSAuthenticator):
         :return: Azure DNS client
         :rtype: DnsManagementClient
         """
-        return DnsManagementClient(self.credential, subscription_id, None, self._arm_endpoint, credential_scopes=[self._arm_endpoint + "/.default"])
+        return DnsManagementClient(CredentialWrapper(self.credential, resource_id=self._arm_endpoint + "/.default"), subscription_id, None, self._arm_endpoint)
