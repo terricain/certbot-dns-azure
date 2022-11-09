c696da3f51ee0eb8527c282c8972f5f56454fc43
Terry Cain <terry@terrys-home.co.uk>
2022-11-09 18:38:41 +0000

    Update setup

--- setup.py
+++ setup.py
@@ -4,29 +4,19 @@ import sys
 from setuptools import find_packages
 from setuptools import setup
 
-version = '1.5.0'
+version = '2.0.0'
 
 # azure-mgmt-dns is still the old style SDK, so will change dramatically
 # when they refactor, most notably the credential parts
 install_requires = [
     'azure-identity>=1.11.0',
     'azure-mgmt-dns>=8.0.0',
-    'setuptools>=39.0.1',
+    'setuptools>=41.6.0',
+    'certbot>=1.18.0,<2.0'
 ]
 
 with open("README.md") as f:
     long_description = f.read()
-
-if not os.environ.get('SNAP_BUILD'):
-    install_requires.extend([
-        'acme>=0.29.0',
-        'certbot>=1.1.0',
-    ])
-elif 'bdist_wheel' in sys.argv[1:]:
-    raise RuntimeError('Unset SNAP_BUILD when building wheels '
-                       'to include certbot dependencies.')
-if os.environ.get('SNAP_BUILD'):
-    install_requires.append('packaging')
 
 docs_extras = [
     'Sphinx>=1.0',  # autodoc_member_order = 'bysource', autodoc_default_flags
